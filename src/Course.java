99a26299372c644eab03202b6dfd1eb7d58ece39
r-meagher <rmeaghe2@uwo.ca>
2017-02-15 13:27:16 -0500

    Added Course class

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Bobby on 2017-02-15.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+        this.cname = cname;
+        this.description = description;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ethan on 2017-02-07.
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
 
     public Student(String name, String emailAddress, String major) {
         super(name, emailAddress);
         this.major = major;
+        courseList = new ArrayList<>();
     }
 
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
+    }
+
+    public void addCourse(Course c) {
+        courseList.add(c);
     }
 }
